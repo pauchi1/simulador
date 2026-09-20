0d12e2b8b9ce4f52b570d2930d8494ec2fd588eb
pauchi1 <p.chiluisa118@gmail.com>
2026-09-20 16:14:18 -0500

    archivos java definidos y función calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    let ingreso = document.getElementById("txtIngresos");
+    let ingresoSTR = ingreso.value;
+    let ingresoFloat = parseFloat(ingresoSTR);
+
+    let egreso = document.getElementById("txtEgresos");
+    let egresoSTR = egreso.value;
+    let egresoFloat = parseFloat(egresoSTR);
+
+    let calcularValor = calcularDisponible(ingresoFloat, egresoFloat);
+    
+    let cmpCalculo = document.getElementById("spnDisponible");
+    cmpCalculo.textContent = calcularValor;
+}

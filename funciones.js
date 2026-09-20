16a3e4d1109c485be058cc9c86da3283490340d5
pauchi1 <p.chiluisa118@gmail.com>
2026-09-20 15:56:11 -0500

    función calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,8 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {
+    let valorDisponible = ingresos - egresos;
+    if (valorDisponible < 0) {
+        valorDisponible = 0;
+    }
+    return valorDisponible;
+}

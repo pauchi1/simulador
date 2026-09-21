bda9b84b995f23926ddaa8d4c00ecad3ced368ff
pauchi1 <p.chiluisa118@gmail.com>
2026-09-21 00:06:31 -0500

    función calcularInteresSimple funcionando

--- simulador.js
+++ simulador.js
@@ -17,4 +17,21 @@ function calcular() {
 
     let cmpPago = document.getElementById("spnCapacidadPago");
     cmpPago.textContent = calcularPago;
+
+    let cmpMonto = document.getElementById("txtMonto");
+    let montoSTR = cmpMonto.value;
+    let montoFloat = parseFloat(montoSTR);
+
+    let cmpPlazo = document.getElementById("txtPlazo");
+    let plazoSTR = cmpPlazo.value;
+    let plazoInt = parseInt(plazoSTR);
+
+    let cmpInteres = document.getElementById("txtTasaInteres");
+    let interesSTR = cmpInteres.value;
+    let interesFloat = parseFloat(interesSTR)
+
+    let calcularInteres = calcularInteresSimple(montoFloat, interesFloat, plazoInt);
+
+    let cmpTotal = document.getElementById("spnInteresPagar");
+    cmpTotal.textContent = calcularInteres;
 }
